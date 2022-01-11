7d0bab278188ef4047d65b76e7712970ecb3d43c
Camila Fonseca <camilarqf@gmail.com>
2022-01-11 19:54:06 -0300

    feat: adiciona classe pessoa

--- src/main/java/br/com/sgb/model/Pessoa.java
+++ src/main/java/br/com/sgb/model/Pessoa.java
@@ -1,2 +1,44 @@
-package br.com.sgb.model;public class Pessoa {
+package br.com.sgb.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public abstract class Pessoa implements Serializable {
+
+    private static final long serialVersionUID = 1751790146106078383L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(nullable = false)
+    private String nome;
+    @CPF(message = "Forneça um CPF válido")
+    @Column(unique = true, nullable = false)
+    private String cpf;
+    @Column(nullable = false)
+    private String telefone;
+    @Email(message = "Forneça um e-mail válido")
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String endereco;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "Perfis")
+    private Set<Integer> perfis = new HashSet<>();
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
+    private String senha;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataCriacao = LocalDate.now();
+
+
 }
