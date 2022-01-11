65ba30cbcb0b170ff13676412ab8a000a9a68782
Camila Fonseca <camilarqf@gmail.com>
2022-01-11 19:05:38 -0300

    commit inicial

---
+++ src/test/java/br/com/sgb/SgbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
