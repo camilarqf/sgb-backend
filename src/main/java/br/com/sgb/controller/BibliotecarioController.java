ec5ba9bcce688aa61a49216452491ef71a3a415f
Camila Fonseca <camilarqf@gmail.com>
2022-01-12 19:48:22 -0300

    fix: corrige controller usuario

---
+++ src/main/java/br/com/sgb/controller/BibliotecarioController.java
@@ -0,0 +1,2 @@
+package br.com.sgb.controller;public class BibliotecarioController {
+}
--- src/main/java/br/com/sgb/controller/UsuarioController.java
+++ src/main/java/br/com/sgb/controller/UsuarioController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping(value = "/usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
