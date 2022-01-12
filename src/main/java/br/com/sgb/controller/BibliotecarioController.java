040d726af7ec3574051f53a19c885598904310c9
Camila Fonseca <camilarqf@gmail.com>
2022-01-12 19:51:51 -0300

    feat: adiciona endpoint findAll bibliotecario

--- src/main/java/br/com/sgb/controller/BibliotecarioController.java
+++ src/main/java/br/com/sgb/controller/BibliotecarioController.java
@@ -1,2 +1,29 @@
-package br.com.sgb.controller;public class BibliotecarioController {
+package br.com.sgb.controller;
+
+import br.com.sgb.dto.BibliotecarioDTO;
+import br.com.sgb.model.Bibliotecario;
+import br.com.sgb.service.BibliotecarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/bibliotecarios")
+public class BibliotecarioController {
+
+    @Autowired
+    private BibliotecarioService bibliotecarioService;
+
+    @GetMapping
+    public ResponseEntity<List<BibliotecarioDTO>> findAll(){
+        List<Bibliotecario> bibliotecarioList = bibliotecarioService.findAll();
+        List<BibliotecarioDTO> bibliotecarioDTOList = bibliotecarioList.stream().map(x -> new BibliotecarioDTO(x))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(bibliotecarioDTOList);
+    }
 }
--- src/main/java/br/com/sgb/service/BibliotecarioService.java
+++ src/main/java/br/com/sgb/service/BibliotecarioService.java
@@ -1,2 +1,20 @@
-package br.com.sgb.service;public class BibliotecarioService {
+package br.com.sgb.service;
+
+import br.com.sgb.model.Bibliotecario;
+import br.com.sgb.repository.BibliotecarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BibliotecarioService {
+
+    @Autowired
+    private BibliotecarioRepository bibliotecarioRepository;
+
+    public List<Bibliotecario> findAll(){
+        return bibliotecarioRepository.findAll();
+    }
+
 }
