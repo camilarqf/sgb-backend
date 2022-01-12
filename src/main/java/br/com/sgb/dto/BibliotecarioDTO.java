90378abb32f109fb4ba4e6accb1c8447cb9ba710
Camila Fonseca <camilarqf@gmail.com>
2022-01-12 18:48:20 -0300

    feat: adiciona dto bibliotecario

---
+++ src/main/java/br/com/sgb/dto/BibliotecarioDTO.java
@@ -0,0 +1,74 @@
+package br.com.sgb.dto;
+
+import br.com.sgb.model.Bibliotecario;
+import br.com.sgb.model.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class BibliotecarioDTO implements Serializable {
+    private static final long serialVersionUID = 4152119063333328088L;
+
+    protected Integer id;
+
+    @NotNull(message = "O campo NOME é obrigatório")
+    protected String nome;
+
+    @NotNull(message = "O campo CPF é obrigatório")
+    protected String cpf;
+
+    @NotNull(message = "O campo MATRÍCULA é obrigatório")
+    protected String matricula;
+
+    @NotNull(message = "O campo TELEFONE é obrigatório")
+    protected String telefone;
+
+    @NotNull(message = "O campo EMAIL é obrigatório")
+    protected String email;
+
+    @NotNull(message = "O campo ENDEREÇO é obrigatório")
+    protected String endereco;
+
+    protected Set<Integer> perfis = new HashSet<>();
+
+    @NotNull(message = "O campo SENHA é obrigatório")
+    protected String senha;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public BibliotecarioDTO() {
+        addPerfil(Perfil.BIBLIOTECARIO);
+    }
+
+    public BibliotecarioDTO(Bibliotecario bibliotecario) {
+        this.id = bibliotecario.getId();
+        this.nome = bibliotecario.getNome();
+        this.cpf = bibliotecario.getCpf();
+        this.matricula = bibliotecario.getMatricula();
+        this.telefone = bibliotecario.getTelefone();
+        this.email = bibliotecario.getEmail();
+        this.endereco = bibliotecario.getEndereco();
+        this.perfis = bibliotecario.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.senha = bibliotecario.getSenha();
+        this.dataCriacao = bibliotecario.getDataCriacao();
+        addPerfil(Perfil.BIBLIOTECARIO);
+    }
+
+    public void addPerfil(Perfil perfil){
+        this.perfis.add(perfil.getCodigo());
+    }
+
+    public Set<Perfil> getPerfis(){
+        return this.perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+}
--- src/main/java/br/com/sgb/model/Bibliotecario.java
+++ src/main/java/br/com/sgb/model/Bibliotecario.java
@@ -1,13 +1,16 @@
 package br.com.sgb.model;
 
+import br.com.sgb.dto.BibliotecarioDTO;
+import br.com.sgb.model.enums.Perfil;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
-@NoArgsConstructor
 @Getter
 @Setter
 public class Bibliotecario extends Pessoa{
@@ -16,5 +19,33 @@ public class Bibliotecario extends Pessoa{
     public Bibliotecario(Integer id, String nome, String cpf, String matricula, String telefone, String email,
                          String endereco, String senha) {
         super(id, nome, cpf, matricula, telefone, email, endereco, senha);
+        addPerfil(Perfil.BIBLIOTECARIO);
     }
+
+    public Bibliotecario() {
+        addPerfil(Perfil.BIBLIOTECARIO);
+    }
+
+    public Bibliotecario(BibliotecarioDTO bibliotecarioDTO) {
+        this.id = bibliotecarioDTO.getId();
+        this.nome = bibliotecarioDTO.getNome();
+        this.cpf = bibliotecarioDTO.getCpf();
+        this.matricula = bibliotecarioDTO.getMatricula();
+        this.telefone = bibliotecarioDTO.getTelefone();
+        this.email = bibliotecarioDTO.getEmail();
+        this.endereco = bibliotecarioDTO.getEndereco();
+        this.perfis = bibliotecarioDTO.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.senha = bibliotecarioDTO.getSenha();
+        this.dataCriacao = bibliotecarioDTO.getDataCriacao();
+        addPerfil(Perfil.BIBLIOTECARIO);
+    }
+
+    public void addPerfil(Perfil perfil){
+        this.perfis.add(perfil.getCodigo());
+    }
+
+    public Set<Perfil> getPerfis(){
+        return this.perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
 }
