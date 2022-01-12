e416fdccddc460f484d38bb059f2d6a7cf5c2895
Camila Fonseca <camilarqf@gmail.com>
2022-01-12 19:03:14 -0300

    feat: adiciona endpoint findAll

---
+++ src/main/java/br/com/sgb/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package br.com.sgb.controller;
+
+import br.com.sgb.dto.UsuarioDTO;
+import br.com.sgb.model.Usuario;
+import br.com.sgb.repository.UsuarioRepository;
+import br.com.sgb.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> findAll(){
+        List<Usuario> usuarioList = usuarioService.findAll();
+        List<UsuarioDTO> usuarioDTOList = usuarioList.stream().map(x -> new UsuarioDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(usuarioDTOList);
+    }
+}
--- src/main/java/br/com/sgb/service/UsuarioService.java
+++ src/main/java/br/com/sgb/service/UsuarioService.java
@@ -1,2 +1,19 @@
-package br.com.sgb.service;public class UsuarioService {
+package br.com.sgb.service;
+
+import br.com.sgb.model.Usuario;
+import br.com.sgb.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
 }
